7b73b4079df9033ec83c4b754439f3311d6fde05
Sunil Kondeti <sunilkondeti234@gmail.com>
2025-06-03 12:50:31 +0530

    Initial working version

---
+++ fixed/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Cloud Kitchen</h1>
+      <p className="text-lg">Your favorite pickles, delivered to your door!</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Products from './Products';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Products />
+    </>
+  );
+}
+
+export default App;
