27b690e65b727cccc5f33c455bf567b220cd1e37
Sunil Kondeti <sunilkondeti234@gmail.com>
2025-06-03 13:56:48 +0530

    Added Navbar and Products components

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Cloud Kitchen</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div>
+      <h2>Our Pickles</h2>
+      <p>Product list coming soon...</p>
+    </div>
+  );
+};
+
+export default Products;
