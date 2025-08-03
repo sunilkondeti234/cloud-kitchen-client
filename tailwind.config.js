141308d4c3bd313f4a310ee3a33fda84d9b468ae
sunilkondeti234 <sunilkondeti234@gmail.com>
2025-08-03 14:03:34 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
   theme: {
     extend: {},
   },
